2dcb7ef52d95aa1ab2d18c039eb1a53866a390c5
nathanshaw <nathanshawsemail@gmail.com>
2018-12-07 14:25:00 -0800

    added different levels of verbose printing in the printing files

--- Matrix/printing.cpp
+++ Matrix/printing.cpp
@@ -31,3 +31,36 @@ void vprintln(int text) {
     Serial.println(text);
   }
 }
+
+///////////////////////////////////////////////////////////////////////////////////////////
+// Overloaded for higher levels of verbosity
+///////////////////////////////////////////////////////////////////////////////////////////
+// verbose printing (basic)
+void vprint(String text, int level) {
+  // TODO update this to use *args and add return line at the end if possible like in python
+  if (VERBOSE > level) {
+    Serial.print(text);
+  }
+}
+
+// verbose printing (basic)
+void vprint(int integer, int level) {
+  // TODO update this to use *args and add return line at the end if possible like in python
+  if (VERBOSE > level) {
+    Serial.print(integer);
+  }
+}
+
+// verbose printing basic return line
+void vprintln(String text, int level) {
+  if (VERBOSE > level) {
+    Serial.println(text);
+  }
+}
+
+// verbose printing basic return line
+void vprintln(int integer, int level) {
+  if (VERBOSE > level) {
+    Serial.println(integer);
+  }
+}
--- Matrix/printing.h
+++ Matrix/printing.h
@@ -4,8 +4,13 @@
 #include <Arduino.h>
 
 void vprint(String);
+void vprint(String, int);
 void vprint(int);
+void vprint(String, int);
+
 void vprintln(String);
+void vprintln(String, int);
 void vprintln(int);
+void vprintln(int, int);
 
 #endif // PRINTING_H
